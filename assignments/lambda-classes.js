e9fb75b154031633338369988cb946b34bf837ca
ShaunKolich <employmeinc@gmail.com>
2019-04-24 22:10:34 -0400

    Halfway through Assignement

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -7,15 +7,49 @@ class Person {
         this.gender = attributes.gender;
     }
     speak() {
-            return `hello my name is ${this.name}, I am from ${this.location}`
-}
+        return `hello my name is ${this.name}, I am from ${this.location}`
+    }
 }
 
-class Instructor extends Person{
+class Instructor extends Person {
     constructor(attributes) {
         super(attributes);
+        this.specialty = attributes.specialty;
+        this.favLanguage = attributes.favLanguage;
+        this.catchPhrase = attributes.catchPhrase;
+        // this.subject = attributes.subject;
+    }
+    demo() {
+        return `Today we are learning about ${subject}`;
+    }
+    grade() {
+        return `${student.name} recieves a perfect score on ${student.className}`;
+    }
+
+}
+
+class Student extends Instructor {
+    constructor(attributes) {
+        super(attributes);
+        this.previousBackground = attributes.previousBackground;
+        this.className = attributes.className;
+        this.favInstructor = attributes.favInstructor;
+    }
+
+
 
 }
 
 
-}
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+});
+
+console.log(fred.speak());
